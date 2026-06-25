1e9726b82832b9e3da68739dea37974e2b6bf1e9
Rakhmonov Sardorbek <sardorbek.rakhmonov@gmail.com>
2026-06-26 00:32:48 +0900

    feat: add initial user model and prisma migration

---
+++ backend-api/prisma/migrations/20260621161640_create_user_model/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'BLOCKED', 'DELETED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "isPhoneVerified" BOOLEAN NOT NULL DEFAULT false,
+    "passwordHash" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+    "avatarUrl" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
